05891320271b171fe5dbb7a6ac69c04dd4180d61
Andrey Kumykov <aakumykov@yandex.ru>
2021-03-15 23:37:56 +0700

    Пробное преобразование из JSON в объект успешно.

---
+++ app/src/main/java/com/github/aakumykov/cc/Currency.java
@@ -0,0 +1,16 @@
+package com.github.aakumykov.cc;
+
+import com.google.gson.annotations.SerializedName;
+
+class Currency {
+
+    @SerializedName("ID") private String id;
+    @SerializedName("Name") private String name;
+    @SerializedName("NumCode") private int numCode;
+    @SerializedName("CharCode") private String charCode;
+    @SerializedName("Nominal") private int nominal;
+    @SerializedName("Value") private float value;
+    @SerializedName("Previous") private float prevValue;
+
+    public Currency() {}
+}
---
+++ app/src/main/java/com/github/aakumykov/cc/CurrencyBoard.java
@@ -0,0 +1,22 @@
+package com.github.aakumykov.cc;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+import java.util.Map;
+
+public class CurrencyBoard {
+
+    @SerializedName("Date") private Date mDate;
+    @SerializedName("PreviousDate") private Date mPrevDate;
+    @SerializedName("PreviousURL") private String mPrevURL;
+    @SerializedName("Timestamp") private Date mTimestamp;
+    @SerializedName("Valute") private Map<String,Currency> mCurrencyMap;
+
+    public CurrencyBoard() {}
+
+    public int getValuteCount() {
+        return mCurrencyMap.size();
+    }
+
+}
